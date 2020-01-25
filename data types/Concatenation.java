96d870b256820d55d7a9b31fb6287f6d74416d28
psbill29 <56553775+psbill29@users.noreply.github.com>
2020-01-25 10:19:02 +0530

    First Time Commit

---
+++ data types/Concatenation.java
@@ -0,0 +1,12 @@
+class Concatenation{
+	public static void main(String[] args){
+	String name="praveen";
+	String place="bengalore"; 
+	System.out.println(name+ " is my name ");
+	System.out.println("hi,this is " +name);
+	System.out.println("hi"+" name");
+	System.out.println("hello "+name+ " is from bengalore");
+	System.out.println("hi "+ "welcome to java");
+	System.out.println(name+place);
+	}
+}
---
+++ data types/Numb.java
@@ -0,0 +1,13 @@
+class Numb{
+	public static void main(String[] args){
+	int a=20;
+	int b=30;
+	System.out.println(a+b);
+	System.out.println(b+a);
+	System.out.println("number1="+a);
+	System.out.println("sum="+(a+b));
+	System.out.println("sum"+a+b);
+	System.out.println(a+b+"=sum");
+	System.out.println(a+"sum"+b);
+	}
+}
